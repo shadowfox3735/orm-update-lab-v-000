828a4083795f916e53d9cebe9d0a4f3b2728c193
shadowfox3735 <normanpan180@gmail.com>
2019-04-12 17:34:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,12 @@ class Student
   end
   
   def update
+    sql = <<-SQL
+    UPDATE students
+    SET name = ?, grade = ? WHERE id =?
+    SQL
     
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
 end
